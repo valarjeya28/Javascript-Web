ff5c9a75ab3a23af9f2a15756c0fbd35d9b49088
valar <valarjeya@gmail.com>
2019-05-21 15:28:38 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,8 +3,6 @@ var tabledata = data;
 
 // Variables
 var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime");
-var inputField2 = d3.select("#city");
 var tbody = d3.select("tbody");
 var resetbtn = d3.select("#reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
@@ -21,3 +19,44 @@ var populate = (dataInput) => {
 //Populate table
 populate(data);
 
+
+function filterTableButton() {
+	
+	d3.event.preventDefault();
+	
+	var searchDate = d3.select("#datetime").property("value");
+  var searchcity=d3.select("#City").property("value");
+  var searchState=d3.select("#State").property("value");
+  var searchCountry=d3.select("#Country").property("value");
+  var searchShape=d3.select("#Shape").property("value");
+
+	var filteredDatas = data;
+
+	if (searchDate != ""){
+    	filteredDatas = filteredDatas.filter(filterdata => filterdata.datetime === searchDate);
+    }
+    if (searchcity !=""){
+    	filteredDatas = filteredDatas.filter(filterdata => filterdata.city.toLowerCase() === searchcity.toLowerCase());
+    }
+    if (searchState !=""){
+      filteredDatas = filteredDatas.filter(filterdata => filterdata.state.toLowerCase() === searchState.toLowerCase());
+      }
+  if (searchCountry !=""){
+      filteredDatas = filteredDatas.filter(filterdata => filterdata.country.toLowerCase() === searchCountry.toLowerCase());
+      }
+  if (searchShape !=""){
+      filteredDatas = filteredDatas.filter(filterdata => filterdata.shape.toLowerCase() === searchShape.toLowerCase());
+      }
+
+    tbody.html('');
+    populate(filteredDatas);
+}
+
+function resetTableButton() {
+	tbody.html('');
+	populate(tabledata);
+}
+
+populate(tabledata);
+button.on("click", filterTableButton );
+resetbtn.on("click", resetTableButton );
